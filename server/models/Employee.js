fe0c5d00416ac46a13240c99b0919f579b17f0f0
KTshibangu <ktshibangu@gmail.com>
2026-05-14 03:38:53 +0200

    APIs created for employees, auth, profile and attendance

--- server/models/Employee.js
+++ server/models/Employee.js
@@ -3,7 +3,8 @@ import { DEPARTMENTS } from "../constants/departments.js";
 
 const employeeSchema = new mongoose.Schema({
     userId: {
-        type: mongoose.Schema.Types.ObjectId, ref: "User",
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User",
         required: true,
         unique: true
     },
